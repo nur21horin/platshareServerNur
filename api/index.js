cbd85c50455733147032ac900046e97cef97f168
Nur Mohammod <nurm98371@gmail.com>
2026-01-08 21:20:16 +0600

    add

--- api/index.js
+++ api/index.js
@@ -11,16 +11,19 @@
 //   }
 // };
 
-require("dotenv").config();
+const serverless = require("serverless-http");
 const app = require("../src/app");
 const { ensureDbConnection } = require("../src/db/mongoClient");
 
-module.exports = async (req, res) => {
+const handler = async (req, res) => {
   try {
-    await ensureDbConnection(); // Make sure MongoDB is ready
-    app(req, res); // Pass the request to your Express app
+    await ensureDbConnection(); // make sure DB is connected
+    return app(req, res);
   } catch (error) {
     console.error("API handler failed:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+// **THIS is the export Vercel needs**
+module.exports = serverless(handler);
