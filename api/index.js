7804e7cce1db5272a66f8626290851f8e7e0077e
Nur Mohammod <nurm98371@gmail.com>
2026-01-08 21:16:13 +0600

    add

--- api/index.js
+++ api/index.js
@@ -1,30 +1,12 @@
-// const app = require("../src/app");
-// const { ensureDbConnection } = require("../src/db/mongoClient");
-
-// module.exports = async (req, res) => {
-//   try {
-//     await ensureDbConnection();
-//     return app(req, res);
-//   } catch (error) {
-//     console.error("API handler failed:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
-
-require("dotenv").config();
-const serverless = require("serverless-http");
 const app = require("../src/app");
 const { ensureDbConnection } = require("../src/db/mongoClient");
 
-const handler = async (req, res) => {
+module.exports = async (req, res) => {
   try {
     await ensureDbConnection();
-    return app(req, res); // Express app works here
+    return app(req, res);
   } catch (error) {
     console.error("API handler failed:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-module.exports = serverless(handler);
-
