132514e3b709f5958fff86ef53da645d86f9a9d5
Nur Mohammod <nurm98371@gmail.com>
2026-01-08 21:18:19 +0600

    ad

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
+// const app = require("../src/app");
+// const { ensureDbConnection } = require("../src/db/mongoClient");
+
+// module.exports = async (req, res) => {
+//   try {
+//     await ensureDbConnection();
+//     return app(req, res);
+//   } catch (error) {
+//     console.error("API handler failed:", error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
+require("dotenv").config();
 const app = require("../src/app");
 const { ensureDbConnection } = require("../src/db/mongoClient");
 
 module.exports = async (req, res) => {
   try {
-    await ensureDbConnection();
-    return app(req, res);
+    await ensureDbConnection(); // Make sure MongoDB is ready
+    app(req, res); // Pass the request to your Express app
   } catch (error) {
     console.error("API handler failed:", error);
     res.status(500).json({ message: "Internal server error" });
